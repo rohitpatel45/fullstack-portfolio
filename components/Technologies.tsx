e03b624640ff504198e3dd64c1787b28deee69a8
SonnySanga <rohit.kumar018219>
2024-02-24 12:29:46 +0530

    added key in tech

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -11,7 +11,10 @@ const Technology = () => {
 
       <div className="bg-[#ff6e7f] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center py-10">
         {technologies.map((technologyy) => (
-          <div className="flex flex-col space-y-3 p-4 h-96 w-80 text-center justify-center gap-2 border-[#e65c00] border-2 m-4 text-white/80 items-center">
+          <div
+            className="flex flex-col space-y-3 p-4 h-96 w-80 text-center justify-center gap-2 border-[#e65c00] border-2 m-4 text-white/80 items-center"
+            key={technologyy.title}
+          >
             <Image src={technologyy.image} alt="" width={200} height={200} />
             <h1>{technologyy.title}</h1>
             <h3>{technologyy.description}</h3>
